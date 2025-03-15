f6317a0d20cc43d297bb94758c7fb0dda8f95de1
Biyabylla <ardinanabila35@gmail.com>
2025-03-15 08:16:54 +0700

    tambah baris kedua

--- src/tugas1sm2/Tugas1SM2.java
+++ src/tugas1sm2/Tugas1SM2.java
@@ -16,6 +16,7 @@ public class Tugas1SM2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        System.out.println("tambah baris kedua");
     }
     
 }
